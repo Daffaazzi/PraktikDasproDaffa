7fc1bfc751857119ba13d4370b1cc84baac12b6b
Daffaazzi <azzidaffa@gmail.com>
2025-09-24 19:51:37 +0700

    ditambah saya ingin belajar git

--- Test.java
+++ Test.java
@@ -1,17 +1,10 @@
-<<<<<<< HEAD
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hallo, project ini sedang di modif oleh user daffa");
         System.out.println("User daffa menambahkan baris kode ini");
+        System.out,println("saaya ingin belajar git")
 
 
     }
     
 }
-=======
-public class Test {
-    public static void main(String[] args) {
-        System.out.println("Hi, ini baris kode pertama"); 
-    }
-}
->>>>>>> 3234304379dc2da11779a4742a2582b9055dcd9e
