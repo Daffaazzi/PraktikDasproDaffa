a8d5a983896de915687fe3799ba138dce4e5c982
Daffaazzi <azzidaffa@gmail.com>
2025-09-24 19:28:40 +0700

    hallo rey

--- Test.java
+++ Test.java
@@ -1,6 +1,8 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hallo, project ini sedang di modif oleh user daffa");
+
+
     }
     
 }
