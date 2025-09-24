ee1fd178ded36ae57ff0e83ee56210dfd787231f
Daffaazzi <azzidaffa@gmail.com>
2025-09-24 19:37:00 +0700

    modifan println

--- Test.java
+++ Test.java
@@ -2,13 +2,14 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hallo, project ini sedang di modif oleh user daffa");
+        System.out.println("User daffa menambahkan baris kode ini");
 
 
     }
     
 }
 =======
-public class test {
+public class Test {
     public static void main(String[] args) {
         System.out.println("Hi, ini baris kode pertama"); 
     }
