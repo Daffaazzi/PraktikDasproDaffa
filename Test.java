46640b34a461c18de02e3443bf6762f693d9e87e
Daffaazzi <azzidaffa@gmail.com>
2025-09-24 20:15:27 +0700

    pesan comit

--- Test.java
+++ Test.java
@@ -2,7 +2,8 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Hallo, project ini sedang di modif oleh user daffa");
         System.out.println("User daffa menambahkan baris kode ini");
-        System.out,println("saaya ingin belajar git")
+        System.out.println("saaya ingin belajar git");
+        System.out.println("saya berasal dari kotabatu");
 
 
     }
