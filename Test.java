7be71b285398a6c0134b7335fb6e0a1a1a23fd7c
Aureylio <aureyliorangga2@gmail.com>
2025-09-24 20:03:50 +0700

    dafa

--- Test.java
+++ Test.java
@@ -1,16 +1,5 @@
-<<<<<<< HEAD
 public class Test {
-    public static void main(String[] args) {
-        System.out.println("Hallo, project ini sedang di modif oleh user daffa ganteng");
-
-
-    }
-    
-}
-=======
-public class test {
     public static void main(String[] args) {
         System.out.println("Hi, ini baris kode pertama"); 
     }
 }
->>>>>>> 3234304379dc2da11779a4742a2582b9055dcd9e
