5065b914bf91c2ac9e2a17f73d84fbc7198133f1
Daffaazzi <azzidaffa@gmail.com>
2025-10-01 22:41:20 +0700

    Tugas 1

--- ifCetakKRS08.java
+++ ifCetakKRS08.java
@@ -1,18 +1,27 @@
 import java.util.Scanner;
 
 public class ifCetakKRS08 {
-    java.util.Scanner  daffa= new java.util.Scanner(System.in);
+    java.util.Scanner daffa = new java.util.Scanner(System.in);
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("---Cetak KRS SAKAD---");
-        System.out.println("apakah UKT sudah lunas? (true/false): ");
+        System.out.print("apakah UKT sudah lunas? (true/false): ");
         boolean uktLunas = sc.nextBoolean();
 
-        if(uktLunas) {
+        if (uktLunas) {
             System.out.println("pembayaran UKT terverifikasi");
             System.out.println("Silakan cetak KRS dan minta tanda tangan DPA");
-
-       
+        } else {
+            System.out.println("Registrasi ditolak, silahkan lunasi UKT terlebih dahulu");
         }
+
+        String hasil = uktLunas 
+            ? "pembayaran UKT terverifikasi\nSilakan cetak KRS dan minta tanda tangan DPA"
+            : "Registrasi ditolak, silahkan lunasi UKT terlebih dahulu";
+
+        System.out.println("\n[Versi Ternary Operator] " + hasil);
+
+        sc.close();
     }
 }
