b6304439a8208f2897b50ec2662c9cf3965afb13
Daffaazzi <azzidaffa@gmail.com>
2025-10-01 20:03:19 +0700

    IfCetakKRSfix

--- ifCetakKRS08.java
+++ ifCetakKRS08.java
@@ -1,3 +1,17 @@
+import java.util.Scanner;
+
 public class ifCetakKRS08 {
-    
+    java.util.Scanner  daffa= new java.util.Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("---Cetak KRS SAKAD---");
+        System.out.println("apakah UKT sudah lunas? (true/false): ");
+        boolean uktLunas = sc.nextBoolean();
+
+        if(uktLunas) {
+            System.out.println("pembayaran UKT terverifikasi");
+            System.out.println("Silakan cetak KRS dan minta tanda tangan DPA");
+
+        }
+    }
 }
