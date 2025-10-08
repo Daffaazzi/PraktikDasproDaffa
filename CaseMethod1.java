c9de5d929d64bfebcd24c24c91a1428fcdba71d6
Daffaazzi <azzidaffa@gmail.com>
2025-10-08 17:05:27 +0700

    Memasukkan data mahasiswa

---
+++ CaseMethod1.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class CaseMethod1 {
+    public static void main(String[] args) {
+      java.util.Scanner input = new java.util.Scanner(System.in);
+
+        System.out.println("=====INPUT DATA MAHASISWA=====");
+        System.out.print("Nama : ");
+        String nama = input.nextLine();
+
+        System.out.print("NIM  : ");
+        String nim = input.nextLine();
+
+       
+
+
+
+    }
+}   
+    
+
