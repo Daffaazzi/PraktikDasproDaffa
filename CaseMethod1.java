f7fec8bcb8e25612d798a5c4ba7f07c38fc0b3a5
Daffaazzi <azzidaffa@gmail.com>
2025-10-09 21:51:17 +0700

    menambahkan nilai Algoritma pemrograman

--- CaseMethod1.java
+++ CaseMethod1.java
@@ -12,26 +12,32 @@ public class CaseMethod1 {
         String nim = input.nextLine();
 
         System.out.println("---MATAKULIAH 1: ALGORIMA DAN PEMROGRAMAN---");
-        System.out.print("Nilai UTS:");
+        System.out.print("Nilai UTS : ");
         String Nilai = input.nextLine();
-        System.out.print("Nilai UAS:");
+        System.out.print("Nilai UAS : ");
         String Nilai1 = input.nextLine();
-        System.out.print("Nilai Tugas:");
+        System.out.print("Nilai Tugas : ");
         String Nilai2 = input.nextLine();
 
         System.out.println("---MATAKULIAH 2: STRUKTUR DATA---");
-        System.out.print("Nilai UTS:");
+        System.out.print("Nilai UTS : ");
         String Nilai3 = input.nextLine();
-        System.out.print("Nilai UAS:");
+        System.out.print("Nilai UAS : ");
         String Nilai4 = input.nextLine();
-        System.out.print("Nilai Tugas:");
+        System.out.print("Nilai Tugas : ");
         String Nilai5 = input.nextLine();
   
+  
+        double utsAP = Double.parseDouble(Nilai);
+        double uasAP = Double.parseDouble(Nilai1);
+        double tugasAP = Double.parseDouble(Nilai2);
+
+        
+        }
     
+
 
 
 
     }
-}   
     
-
